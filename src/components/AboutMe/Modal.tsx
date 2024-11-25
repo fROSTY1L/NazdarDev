a470e216420cd067332b2fb87372d6cf82fc39e6
fROSTY1L <124553997+fROSTY1L@users.noreply.github.com>
2024-11-25 20:32:43 +0300

    new modal + animation for bg

--- src/components/AboutMe/Modal.tsx
+++ src/components/AboutMe/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { ContactBar, ContactBarText, ContactBarWrap, ContactIcon, ModalBackground, ModalContainer } from '../../styles/Modal.style';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
